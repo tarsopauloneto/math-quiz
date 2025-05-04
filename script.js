ec120b99cb2d569ea610e3f8bf0222ef5bbe3f35
João Lucas Lima Maia <160551361+joalucaslm@users.noreply.github.com>
2025-05-04 18:06:32 -0300

    feat: gallows suport

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let contador = 0;
-let pontos = 0;
+let score = 0;
+let wrongAnswear = 0;
+
+const imagensForca = [
+  "gallowsSupport.png",
+  "headGallows.png",
+  "chestGallows.png",
+  "legsGallows.png",
+  "bodyGallows.png",
+  "completedGallows.png",
+];
 
 let perguntas = [
   {
-    pergunta: "O arco-íris é composto de 1 cor?",
-    true: "sim",
-    false: "não",
+    pergunta:
+      "Sejam os conjuntos A = {1,2,3} e B = {3,4,5}. Qual a interseção entre os conjuntos A e B? ",
+    true: "{3}",
+    false: "{1,2,3,4,5}",
   },
   {
     pergunta: "Qual planeta que nós vivemos?",
@@ -25,32 +36,50 @@ function mostrarPergunta(index) {
   if (Math.random() < 0.5) {
     document.getElementById("firstAnswear").textContent = respostas[0];
     document.getElementById("secondAnswear").textContent = respostas[1];
+    document.getElementById("score").textContent = "Pontos: " + score;
+    document.getElementById("wrongAnswear").textContent =
+      "Erros: 3/" + wrongAnswear;
     posicaoCorreta = 0;
   } else {
     document.getElementById("firstAnswear").textContent = respostas[1];
     document.getElementById("secondAnswear").textContent = respostas[0];
+    document.getElementById("score").textContent = "Pontos: " + score;
+    document.getElementById("wrongAnswear").textContent =
+      "Erros: 3/" + wrongAnswear;
     posicaoCorreta = 1;
   }
 }
 
 function verificarResposta(respostaUsuario) {
   if (respostaUsuario === posicaoCorreta) {
-    pontos++;
-    console.log("Correct! Pontos:", pontos);
+    score++;
+    console.log("Correct! score:", score);
   } else {
-    console.log("Wrong! Pontos:", pontos);
+    wrongAnswear++;
+    console.log("Wrong! score:", score);
+
+    if (wrongAnswear < imagensForca.length) {
+      document.getElementById("gallowImage").src = `./assets/${imagensForca[wrongAnswear]}`;
+    }
+
+    if (wrongAnswear == 5) {
+      setTimeout(() => {
+        alert("Game Over");
+      }, 100); 
+    }
   }
 
   contador = (contador + 1) % perguntas.length;
   mostrarPergunta(contador);
 }
 
+
 mostrarPergunta(contador);
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowUp") {
+  if (event.key === "ArrowLeft") {
     verificarResposta(0);
-  } else if (event.key === "ArrowDown") {
+  } else if (event.key === "ArrowRight") {
     verificarResposta(1);
   }
 });
